e0a611210e970afc3757ab9c94d354f67003c8d3
Thomas Garenne <thomas.garenne@outlook.com>
2023-01-09 12:33:14 +0100

    create basic structure html css js

---
+++ script.js
@@ -0,0 +1,11 @@
+const total1 = document.getElementById('total1');
+const total2 = document.getElementById('total2');
+
+const current1 = document.getElementById('current1');
+const current2 = document.getElementById('current2');
+
+const newGame = document.getElementById('newGame');
+const dice = document.getElementById('dice');
+const hold = document.getElementById('hold');
+
+const diceContain = document.getElementById('dice-contain');
